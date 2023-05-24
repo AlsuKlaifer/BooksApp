bf297075c25c0bedd369b15d95585842db46a951
Алсу Фаизова <silicium.xxx@gmail.com>
2023-05-24 12:07:27 +0300

    fixed design

--- BooksApp/Modules/BookDescription/View/DescriptionViewController.swift
+++ BooksApp/Modules/BookDescription/View/DescriptionViewController.swift
@@ -23,6 +23,8 @@ class DescriptionViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Outlets
+    
     lazy var imageView: UIImageView = {
         let image = UIImageView()
         image.clipsToBounds = true
@@ -37,7 +39,6 @@ class DescriptionViewController: UIViewController {
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .label
-        label.text = "Book title".uppercased()
         label.font = UIFont.systemFont(ofSize: 25, weight: .bold)
         label.numberOfLines = 0
         label.textAlignment = .center
@@ -48,7 +49,6 @@ class DescriptionViewController: UIViewController {
     lazy var authorLabel: UILabel = {
         let label = UILabel()
         label.textColor = .gray
-        label.text = "Author"
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 20, weight: .light)
         return label
@@ -57,7 +57,7 @@ class DescriptionViewController: UIViewController {
     lazy var favoriteButton: UIButton = {
         let button = UIButton()
         let imageName = output.isFavorite ? "bookmark.fill" : "bookmark"
-        let config = UIImage.SymbolConfiguration(pointSize: .maximum(30, 30), weight: .bold, scale: .large)
+        let config = UIImage.SymbolConfiguration(pointSize: .maximum(30, 30), weight: .medium, scale: .large)
         button.setImage(UIImage(systemName: imageName, withConfiguration: config), for: .normal)
         button.tintColor = .systemYellow
         button.addTarget(self, action: #selector(favoriteButtonTapped), for: .touchUpInside)
@@ -66,23 +66,64 @@ class DescriptionViewController: UIViewController {
         button.heightAnchor.constraint(equalToConstant: 100).isActive = true
         return button
     }()
-
-    lazy var shareButton: UIButton = {
-        let button = RoundButton()
-        let config = UIImage.SymbolConfiguration(pointSize: .zero, weight: .bold, scale: .large)
-        button.setImage(UIImage(systemName: "square.and.arrow.up", withConfiguration: config), for: .normal)
-//        button.addTarget(self, action: #selector(readButtonTapped), for: .touchUpInside)
-        return button
+    
+    // Info View
+    private lazy var infoView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 15
+        view.clipsToBounds = true
+        view.layer.borderWidth = 2
+        view.layer.borderColor = UIColor.black.cgColor
+        return view
     }()
 
-    lazy var downloadButton: UIButton = {
-        let button = RoundButton()
-        let config = UIImage.SymbolConfiguration(pointSize: .zero, weight: .bold, scale: .large)
-        button.setImage(UIImage(systemName: "house", withConfiguration: config), for: .normal)
-//        button.addTarget(self, action: #selector(readButtonTapped), for: .touchUpInside)
-        return button
+    lazy var pagesStack = createStackView(pagesTitle, pagesLabel)
+    
+    lazy var pagesTitle = createTitleLabel("Pages")
+    
+    lazy var pagesLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        guard let pages = output.book.volumeInfo.pageCount else {
+            label.text = "-"
+            return label
+        }
+        label.text = "\(pages)"
+        return label
     }()
-
+    
+    lazy var ratingStack = createStackView(ratingTitle, ratingLabel)
+    
+    lazy var ratingTitle = createTitleLabel("Rating")
+    
+    lazy var ratingLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        guard let rating = output.book.volumeInfo.averageRating else {
+            label.text = "-"
+            return label }
+        label.text = "\(rating)"
+        return label
+    }()
+    
+    lazy var languageStack = createStackView(languageTitle, languageLabel)
+    
+    lazy var languageTitle = createTitleLabel("Language")
+    
+    lazy var languageLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.text = String(describing: output.book.volumeInfo.language).uppercased()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
     lazy var readButton: UIButton = {
         let button = UIButton()
         let title = output.isRead ? "Remove from read" : "Add to read"
@@ -98,6 +139,8 @@ class DescriptionViewController: UIViewController {
         return button
     }()
 
+    // MARK: - Buttons Action
+    
     @objc func favoriteButtonTapped() {
         let config = UIImage.SymbolConfiguration(pointSize: .maximum(30, 30), weight: .bold, scale: .large)
         if output.isFavorite {
@@ -133,61 +176,98 @@ class DescriptionViewController: UIViewController {
         reloadData()
     }
 
+    // MARK: - Constraints
+    
     func setConstraints() {
         view.addSubview(imageView)
-        view.addSubview(titleLabel)
-        view.addSubview(authorLabel)
-        view.addSubview(readButton)
-        view.addSubview(favoriteButton)
-
-        let stackview = UIStackView(arrangedSubviews: [downloadButton, shareButton])
-
-        stackview.axis = .horizontal
-        stackview.spacing = 40
-        stackview.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackview)
-
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             imageView.heightAnchor.constraint(equalToConstant: 200),
             imageView.widthAnchor.constraint(equalToConstant: 150)
         ])
-
+        
+        view.addSubview(favoriteButton)
         NSLayoutConstraint.activate([
             favoriteButton.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 30),
             favoriteButton.topAnchor.constraint(equalTo: imageView.topAnchor, constant: -60)
         ])
 
+        view.addSubview(titleLabel)
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
             titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
             titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 40)
         ])
-
+        
+        view.addSubview(authorLabel)
         NSLayoutConstraint.activate([
             authorLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
             authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10)
         ])
-
+        
+        view.addSubview(infoView)
         NSLayoutConstraint.activate([
-            stackview.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            stackview.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 30)
+            infoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            infoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            infoView.heightAnchor.constraint(equalToConstant: 90),
+            infoView.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 30)
         ])
 
+        infoView.addSubview(pagesStack)
+        NSLayoutConstraint.activate([
+            pagesStack.leadingAnchor.constraint(equalTo: infoView.leadingAnchor, constant: 20),
+            pagesStack.topAnchor.constraint(equalTo: infoView.topAnchor, constant: 20)
+        ])
+
+        infoView.addSubview(ratingStack)
+        NSLayoutConstraint.activate([
+            ratingStack.centerXAnchor.constraint(equalTo: infoView.centerXAnchor, constant: 0),
+            ratingStack.topAnchor.constraint(equalTo: infoView.topAnchor, constant: 20)
+        ])
+        
+        infoView.addSubview(languageStack)
+        NSLayoutConstraint.activate([
+            languageStack.trailingAnchor.constraint(equalTo: infoView.trailingAnchor, constant: -20),
+            languageStack.topAnchor.constraint(equalTo: infoView.topAnchor, constant: 20)
+        ])
+        
+        view.addSubview(readButton)
         NSLayoutConstraint.activate([
             readButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
-            readButton.topAnchor.constraint(equalTo: stackview.bottomAnchor, constant: 40)
+            readButton.topAnchor.constraint(equalTo: infoView.bottomAnchor, constant: 40)
         ])
     }
 }
 
 extension DescriptionViewController: DescriptionViewInput {
+    
     func reloadData() {
         let book = output.book
         titleLabel.text = book.volumeInfo.title
         authorLabel.text = book.volumeInfo.authors?[0]
         imageView.downloadImage(from: book.volumeInfo.imageLinks.thumbnail)
+    }
+}
+
+extension DescriptionViewController {
+    
+    func createStackView(_ title: UILabel, _ value: UILabel) -> UIStackView {
+        let stack = UIStackView(arrangedSubviews: [title, value])
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.alignment = .center
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }
+    
+    func createTitleLabel(_ title: String) -> UILabel {
+        let label = UILabel()
+        label.textColor = .label
+        label.text = title
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 16, weight: .light)
+        return label
     }
 }
